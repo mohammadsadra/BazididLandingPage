c693b74343253644cb6371253f63e2fd2a705e8b
Mohammad Sadra Haeri <43936083+mohammadsadra@users.noreply.github.com>
2023-10-14 10:58:03 +0330

    Added modal to project.

--- pages/registration/js/main.js
+++ pages/registration/js/main.js
@@ -8,6 +8,7 @@ const chipsArray = Array(false, false, false, false);
 
 $(document).ready(function () {
     hideLoading();
+    openModal();
 
     // BUTTONS ASSIGN
     for (let i = 1; i <= statesNumber; i++) {
@@ -397,4 +398,33 @@ function showLoading(){
 
 }
 
+/////////////////////////////////////////////////////////////////////////////
+
+//////////////////////////// MODAL FUNCTIONS ////////////////////////////
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+function openModal() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
 /////////////////////////////////////////////////////////////////////////////
